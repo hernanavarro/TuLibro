3f73c1ca020b5c5760df66157f862406492c7eb6
Hernan <navarro.hernan97@gmail.com>
2021-08-05 23:44:51 -0300

    Primer commit

---
+++ src/pages/Home/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from './styled';
+
+const Home = () => {
+    return (
+        <View>
+            <Text>Hector Trolo</Text>
+        </View>
+    );
+}
+
+export default Home;
---
+++ src/pages/Home/styled.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const View = styled.div`
+    width: 100vh;
+    height: 100vw;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: #000;
+`;
+
+export const Text = styled.p`
+    font-size: 20px;
+    font-weight: bold;
+    color: #fff;
+`;
